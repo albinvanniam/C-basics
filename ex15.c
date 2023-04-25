e595e2a9dcd2dc5b47d505fc3c1fdb1fb3c878da
Albin V <89841136+albinvanniam@users.noreply.github.com>
2023-04-25 14:11:49 +0530

    Add files via upload

---
+++ ex15.c
@@ -0,0 +1,38 @@
+//menu ins del exit
+#include<stdio.h>
+#define size 100
+void main()
+{
+	int a[size],n,ch,c,b;
+	printf("Enter the size and elements:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("\n1.updations.\n2.delestions.\nEnter the choice:");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:	printf("Enter the index and element:");
+			scanf("%d %d",&c,&b);
+			for(int i=n;i>=c;--i)
+			{
+				a[i+1]=a[i];
+			}
+			a[c]=b;
+			n++;
+			break;
+		case 2:	printf("Enter the index of element:");
+			scanf("%d",&c);
+			for(int i=c;i<n+1;i++)
+			{
+				a[i]=a[i+1];
+			}
+			n--;
+	}
+	for(int i=0;i<n;i++)
+	{
+		printf("the elemnt is %d\n",a[i]);
+	}
+}
---
+++ matrixsumtranspose17.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+void main()
+{
+	int a[100][100],d=0,r,c,rs=0,cs=0,ch,x;
+	printf("Enter the no of rows and columns:");
+	scanf("%d%d",&r,&c);
+	printf("Enter the elements of array.");	
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(int i=0;i<r;i++)
+	{
+		printf("\n");	
+		for(int j=0;j<c;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+	}
+	printf("\n1.Row sum\n2.Column sum\n3.Diagonals sum\n4.Tranpose\nEnter the option:");
+	scanf("%d",&ch);
+	if(ch==3)
+	{
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				if(i==j)
+				{
+					d+=a[i][j];
+				}
+			}
+		}
+		printf("\nThe diagonals sum is %d",d);		
+	}
+	else if(ch==1)
+	{
+		printf("Enter the row.");
+		scanf("%d",&x);
+		for(int i=0;i<c;i++)
+		{
+			rs+=a[x-1][i];
+		}
+		printf("The required sum is %d",rs);
+	}
+	else if(ch==2)
+	{
+		printf("Enter the column.");
+		scanf("%d",&x);
+		for(int i=0;i<r;i++)
+		{
+			cs+=a[i][x-1];
+		}
+		printf("The required sum is %d",cs);
+	}
+	else
+	{	
+		int t[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				t[i][j]=a[j][i];
+			}
+		}
+		printf("The transpose is \n");
+		for(int i=0;i<r;i++)
+		{	
+			printf("\n");			
+			for(int j=0;j<c;j++)
+			{ 
+ 				printf("%d\t",t[i][j]);
+
+			}
+		}
+
+	}	
+}
